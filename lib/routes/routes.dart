a66e1417ab6347ee0b5c41129d4f91fe48475965
Bytor25 <juan.hincapie0078@uco.net.co>
2026-09-08 20:48:10 -0500

    Se inicia configuración de home temporal.

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -23,8 +23,13 @@ import 'package:machuco/views/product/product_list_page.dart';
 import 'package:machuco/views/review/review_administration_page.dart';
 import 'package:machuco/views/room/room_view_models.dart';
 
-abstract final class AppRoutes {
+import 'package:machuco/temporal_home_page.dart';
+
+abstract final class  AppRoutes {
   static const home = '/';
+
+  static const temporalHome = '/temporal';
+
   static const paymentConfirmation = '/payment/client/confirmation';
   static const clientPayments = '/payment/client/history';
   static const ownerPayments = '/payment/owner';
@@ -52,7 +57,7 @@ abstract final class AppRoutes {
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     final Widget page = switch (settings.name) {
-      home => ClientMotelsPage(),
+      home => TemporalHomePage(),
       clientPayments => const ClientPaymentsPage(),
       ownerPayments => const OwnerPaymentsPage(),
       adminPayments => const AdminFinancePage(),
