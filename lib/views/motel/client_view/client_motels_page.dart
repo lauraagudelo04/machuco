646466608702411df7966e5abd56465f348f376e
migue8104 <miguelangeljaramilloarenas6@gmail.com>
2026-09-09 19:21:36 -0500

    creacion de todas las vistas para el manejo de clientes con controller y datos simulados

--- lib/views/motel/client_view/client_motels_page.dart
+++ lib/views/motel/client_view/client_motels_page.dart
@@ -89,7 +89,7 @@ class _ClientMotelsPageState extends State<ClientMotelsPage> {
             child: AppIconButton(
               icon: Icons.person_outline,
               tooltip: 'Perfil',
-              onPressed: () {},
+              onPressed: () => Navigator.pushNamed(context, AppRoutes.clientProfile),
             ),
           ),
         ],
